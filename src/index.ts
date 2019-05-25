9cc66af4cee2b2a05ec9577aee010ffff1003143
glixlur <itaieps@fastmail.fm>
2019-05-25 18:56:46 -0400

    Updates comments in code

--- src/index.ts
+++ src/index.ts
@@ -1,65 +1,86 @@
-import * as fs from "fs-extra";
-import { resolve, relative, dirname, basename } from "path";
-import _ from "lodash";
-import glob from "glob-promise";
-import { parse } from "@babel/parser";
-import * as t from "@babel/types";
-import { findFirstArgs } from "./babel";
+import * as fs from "fs-extra"
+import _ from "lodash"
+import glob from "glob-promise"
+import { parse } from "@babel/parser"
+import * as t from "@babel/types"
+import { findFirstArgs } from "./babel"
+import { resolve, relative, dirname, basename } from "path"
 
 const DUMMY_FILE = '\0empty'
 
 interface File {
-  filename: string;
-  content: string;
-  provides: string[];
-  requires: string[];
-}
-
-function asyncMap<T, R>(list: T[], fn: (value: T) => Promise<R>) {
-  return Promise.all(list.map(fn));
+  filename: string
+  content: string
+  provides: string[]
+  requires: string[]
 }
 
 /**
- * Returns a Map of file name → file contents.
+ * @example
+ * asyncFlatMap :: (a -> Promise (Either b List b)) -> List a -> Promise List b
  */
-async function loadProject(...pattern: string[]) {
-  const filenames = (await asyncMap(pattern, glob)).flat();
-  const files = await asyncMap(filenames, filename => fs.readFile(filename, "utf8"))
-  return _.zipObject(filenames, files);
+function asyncFlatMap<T, R>(fn: (value: T) => Promise<R[] | R>, list: T[]) {
+  return Promise.all(list.map(fn)).then(arr => arr.flat() as R[])
 }
 
-function matchGroup2(ast: t.File, memberList: string) {
+/**
+ * Returns a Dict of file name → file contents.
+ */
+async function loadProject(...pattern: string[]) {
+  const filenames = await asyncFlatMap(glob, pattern)
+  const files = await asyncFlatMap(filename => fs.readFile(filename, "utf8"), filenames)
+  return _.zipObject(filenames, files)
+}
+
+/**
+ * Returns the first StringLiteral in all functions calls to `memberList`.
+ * @example goog.require("module") → 'google.require' → ["module"]
+ */
+function matchCallExpression(ast: t.File, memberList: string) {
   return findFirstArgs({ ast, memberList, filter: t.isStringLiteral, map: t => t.value })
 }
 
-export async function main(args: string[]) {
-  const src = resolve(__dirname, "../closure-library/closure/goog");
-  const dest = resolve(__dirname, "../lib/goog");
+/**
+ * Parse a JavaScript file to AST and provide useful error messages if failed.
+ */
+function toAST(content: string, filename: string) {
+  try {
+    return parse(content, { sourceType: /export /.test(content) ? "module" : "script" })
+  } catch (e) {
+    e.filename = filename
+    throw e
+  }
+}
 
-  const project = await loadProject(src + "/**/*.js");
+export async function main(args: string[]) {
+  const src = resolve(__dirname, "../closure-library/closure/goog")
+  const dest = resolve(__dirname, "../lib/goog")
+
+  const project = await loadProject(src + "/**/*.js")
 
   let files = _.map(project, (content, filename): File => {
-    let ast: t.File;
-    try {
-      ast = parse(content, { sourceType: /export /.test(content) ? "module" : "script" })
-    } catch (e) {
-      e.filename = filename;
-      throw e;
-    }
+    const ast = toAST(content, filename)
     return {
       filename,
       content,
       provides: (
-        matchGroup2(ast, 'goog.provide') ||
-        matchGroup2(ast, 'goog.module') || []
+        matchCallExpression(ast, 'goog.provide') ||
+        matchCallExpression(ast, 'goog.module') || []
       ),
       requires: (
-        matchGroup2(ast, 'goog.require') || []
+        matchCallExpression(ast, 'goog.require') || []
       ),
     }
   })
+  
+  files = _.filter(files, _.negate(({ filename, requires, provides }) =>
+    basename(filename) === "base.js" ||
+    basename(filename) === "goog.js" ||
+    requires.some(item => item.includes("..")) ||
+    provides.some(item => item.includes(".."))
+  ))
 
-  const baseJs = resolve(src, 'base.js');
+  const baseJs = resolve(src, 'base.js')
 
   files.push({
     filename: DUMMY_FILE,
@@ -72,41 +93,37 @@ export async function main(args: string[]) {
     provides: [baseJs],
     requires: [],
   })
-  
-  files = _.filter(files, _.negate(({ filename, requires, provides }) =>
-    basename(filename) === "/base.js" ||
-    basename(filename) === "/goog.js" ||
-    requires.some(item => item.includes("..")) ||
-    provides.some(item => item.includes(".."))
-  ));
 
   files = _.filter(files, ({ provides }) => provides.length > 0)
 
   for (const f of files) {
-    if (f.filename === DUMMY_FILE) continue;
+    if (f.filename === DUMMY_FILE) continue
 
-    const path = f.filename.replace(src, dest);
+    const path = f.filename.replace(src, dest)
     const require = _([baseJs]).concat(f.requires).flatMap(require =>
       _.filter(files, ({ provides }) => provides.includes(require))!
-    ).map(({ filename }) =>
+    ).map(
+      'filename'
+    ).without(
+      f.filename
+    ).map(filename =>
       relative(dirname(f.filename), filename)
-    ).filter(filename =>
-      filename != basename(f.filename)
     ).map(filename =>
       filename.replace(/\.js$/, "")
     ).map(filename =>
       filename.startsWith(".") ? filename : `./${filename}`
     ).uniq()
+     .filter(n => !n.endsWith(DUMMY_FILE))
      .value()
 
     const importStatements = require.map(f => `import ${JSON.stringify(f)}`)
     // First one is always base.js file
-    importStatements[0] = `import { goog } from ${require[0]}`
+    importStatements[0] = `import { goog } from ${JSON.stringify(require[0])}`
 
-    await fs.ensureDir(dirname(path));
+    await fs.ensureDir(dirname(path))
     await fs.writeFile(path,
       importStatements.join('\n') + '\n\n' + f.content +
       (f.filename === baseJs ? "\nexport { goog } " : "")
-    );
+    )
   }
 }
